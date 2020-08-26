f63001bfc36e657680bdf161ba9ef3a6ee96b222
Glenn McKnight <glenn.mcknight@students.plymouth.ac.uk>
2020-08-26 15:41:19 +0100

    Created controller classes, Added login strategies
    Added controller classes including singleton and imlpementation interface methods.
    Added login strategies to load each type of controller and view.

--- SOFT252_Referral_PatientManagementSystem/src/Main/Main.java
+++ SOFT252_Referral_PatientManagementSystem/src/Main/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package soft252_referral_patientmanagementsystem;
+package Main;
 
 /**
  *
  * @author Glenn McKnight
  */
-public class SOFT252_Referral_PatientManagementSystem {
+public class Main {
 
     /**
      * @param args the command line arguments
